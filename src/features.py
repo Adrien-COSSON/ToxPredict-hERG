9b6cacd94074b5b7da9478cce01d2d681c5a88b6
adriencosson-beep <adrien.cosson@gmail.com>
2026-07-23 01:38:05 +0200

    feat: add new features in features.py

--- src/features.py
+++ src/features.py
@@ -21,7 +21,7 @@ def features_engineering():
     
     # Import dataset from csv file
     input_path = Path(__file__).parent.parent / 'data' / 'raw' / 'herg_cleaned.csv'
-    df = pd.read_csv(input_path)
+    df = pd.read_csv(input_path, index_col='molecule_chembl_id')
     
     # Parsing SMILES into RDKit Mol objects and computing Lipinski descriptors
     df['smiles']    = df['canonical_smiles'].apply(lambda x: Chem.MolFromSmiles(x))
@@ -31,6 +31,19 @@ def features_engineering():
     df['HBA']       = df['smiles'].apply(lambda x: Descriptors.NumHAcceptors(x))
     df['TPSA']      = df['smiles'].apply(lambda x: Descriptors.TPSA(x))
     
+    # Computing additional molecular descriptors relevant for hERG binding (aromaticity, flexibility, charge)
+    df['RotBonds']      = df['smiles'].apply(lambda x: Descriptors.NumRotatableBonds(x))
+    df['AromaticRings'] = df['smiles'].apply(lambda x: Descriptors.NumAromaticRings(x))
+    df['RingCount']     = df['smiles'].apply(lambda x: Descriptors.RingCount(x))
+    df['FractionCSP3']  = df['smiles'].apply(lambda x: Descriptors.FractionCSP3(x))
+    df['HetAtoms']      = df['smiles'].apply(lambda x: Descriptors.NumHeteroatoms(x))
+    df['MaxCharge']     = df['smiles'].apply(lambda x: Descriptors.MaxPartialCharge(x))
+    df['MinCharge']     = df['smiles'].apply(lambda x: Descriptors.MinPartialCharge(x))
+    df['MolRefractivity']  = df['smiles'].apply(lambda x: Descriptors.MolMR(x))
+    df['Complexity']  = df['smiles'].apply(lambda x: Descriptors.BertzCT(x))
+    df['KierAlpha']  = df['smiles'].apply(lambda x: Descriptors.HallKierAlpha(x))
+    df['RadicalElectrons']  = df['smiles'].apply(lambda x: Descriptors.NumRadicalElectrons(x))
+    
     # Computing Morgan fingerprints (ECFP4, 2048-bit) using MorganGenerator
     generator = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=2048)
     df['Morgan_FP'] = df['smiles'].apply(lambda x: generator.GetFingerprint(x))
@@ -38,7 +51,7 @@ def features_engineering():
     # Exporting featured dataset
     output_path = Path(__file__).parent.parent / 'data' / 'raw' / 'herg_featured.csv'
     df.to_csv(output_path)
-    
+    print("export done!")
     return df
 
 if __name__ == "__main__":
